b9ea1a15f62794999dff66ab07bc0dd75af6a0a7
mikezxc136 <vantuan1362002@gmail.com>
2024-06-15 23:50:46 +0700

    Update

--- main.py
+++ main.py
@@ -8,20 +8,28 @@ from sqlalchemy import create_engine
 
 sys.path.append(r"D:\Github Mikezxc\final\utils1")
 
-from data_processing import (Date_Dimension_data,
-                             Exception_Stock_Trading_Facts_data,
-                             Stock_Dimension_data, Stock_Trading_Facts_data)
-from db_utils import create_connection
-from handle_json import load_db_config
-from sql_utils import execute_sql_scripts
+from utils1.data_processing import (Date_Dimension_data,
+                                    Exception_Stock_Trading_Facts_data,
+                                    Stock_Dimension_data,
+                                    Stock_Trading_Facts_data)
+from utils1.db_utils import create_connection
+from utils1.handle_json import load_db_config
+from utils1.sql_utils import execute_sql_scripts
 
 sys.stdout.reconfigure(encoding='utf-8')
-
 
 def main():
     # Provide the absolute path to the config file
     config_file_path = r"D:\Github Mikezxc\final\config\env.json"
-    sql_scripts_directory = r"D:\Github Mikezxc\final\sql_script"
+    
+    # List các file SQL cần thực thi
+    pre_sql_scripts = [
+        r"D:\Github Mikezxc\final\sql_script\create_schema.sql",
+    ]
+    
+    post_sql_scripts = [
+        r"D:\Github Mikezxc\final\sql_script\alter_table.sql",
+    ]
     
     # Đọc cấu hình cơ sở dữ liệu
     config = load_db_config(config_file=config_file_path)
@@ -30,17 +38,17 @@ def main():
     connection_string = create_connection(config)
     engine = create_engine(connection_string)
     
-    # Kết nối tới cơ sở dữ liệu PostgreSQL và tạo schema nếu chưa tồn tại
+    # Kết nối tới cơ sở dữ liệu PostgreSQL và thực thi các script SQL
     connection = None
     try:
         connection = psycopg2.connect(connection_string)
         cursor = connection.cursor()
         
-        # Thực thi tất cả các script SQL trong thư mục
-        execute_sql_scripts(cursor, sql_scripts_directory)
+        # Thực thi các script SQL trước khi chèn dữ liệu
+        execute_sql_scripts(cursor, pre_sql_scripts)
         
         connection.commit()
-        print("Tất cả script SQL đã được thực thi thành công")
+        print("Tất cả script SQL trước khi chèn dữ liệu đã được thực thi thành công")
         
         # Chèn dữ liệu vào các bảng trong schema 'stock'
         Date_Dimension_data.to_sql('Dim_Date', engine, schema='stock', if_exists='append', index=False)
@@ -49,6 +57,12 @@ def main():
         Exception_Stock_Trading_Facts_data.to_sql('Fact_Stock', engine, schema='stock', if_exists='append', index=False)
         
         print("Đã truyền dữ liệu thành công vào các bảng trong schema 'stock'")
+        
+        # Thực thi các script SQL sau khi chèn dữ liệu
+        execute_sql_scripts(cursor, post_sql_scripts)
+        
+        connection.commit()
+        print("Tất cả script SQL sau khi chèn dữ liệu đã được thực thi thành công")
         
     except Exception as e:
         print(f"Lỗi khi kết nối hoặc thực thi lệnh SQL: {e}")
--- utils1/sql_utils.py
+++ utils1/sql_utils.py
@@ -1,11 +1,12 @@
 import os
 
 
-def execute_sql_scripts(cursor, directory):
-    for filename in os.listdir(directory):
-        if filename.endswith(".sql"):
-            file_path = os.path.join(directory, filename)
-            with open(file_path, 'r') as file:
-                sql_script = file.read()
-                cursor.execute(sql_script)
-                print(f"Executed {filename} successfully.")
+def execute_sql_scripts(cursor, script_files):
+    for file_path in script_files:
+        with open(file_path, 'r') as file:
+            sql_script = file.read()
+            cursor.execute(sql_script)
+            print(f"Executed {file_path} successfully.")
+
+
+
